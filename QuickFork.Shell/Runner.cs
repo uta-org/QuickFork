95c5e0b933e5877086571bd9a50bbe7d0146f9d6
Ikillnukes <ikillnukes1@gmail.com>
2019-01-22 19:22:01 +0100

    Added Console.Read() at the end of the program execution

--- QuickFork.Shell/Runner.cs
+++ QuickFork.Shell/Runner.cs
@@ -1,10 +1,14 @@
-﻿namespace QuickFork.Shell
+﻿using System;
+
+namespace QuickFork.Shell
 {
     internal class Runner
     {
         private static void Main(string[] args)
         {
             new DemoProgram().Run();
+
+            Console.Read();
         }
     }
 }
